129a6a0b9a28ba42e243637f20a4c97a4029a028
Joan <jcollanteperez@gmail.com>
2025-08-31 23:26:40 -0500

    progreso del crud productos

--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+def create(request):
+    return render(request, 'create.html')
+def edit(request):
+    return render(request, 'edit.html')
